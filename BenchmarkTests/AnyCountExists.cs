df71a9990b3fb81fdd29465210971f711e359470
Eliseu Mendes de Morais <seoeliseu@gmail.com>
2024-07-02 20:44:47 -0300

    Add project files.

---
+++ BenchmarkTests/AnyCountExists.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+
+namespace BenchmarkTests
+{
+    public class AnyCountExists
+    {
+        public static void ExecuteTest()
+        {
+            TestWithNumberOfRegister(10000);
+            TestWithNumberOfRegister(100000);
+            TestWithNumberOfRegister(1000000);
+            TestWithNumberOfRegister(10000000);
+        }
+
+        private static void TestWithNumberOfRegister(int number)
+        {
+            var funcionarios = new List<Funcionario>();
+            for (int i = 0; i < number; i++)
+            {
+                funcionarios.Add(new Funcionario("Funcionario " + i, i, 1000 + i, DateTime.Now.AddYears(-10), "Endereco " + i, "Bairro " + i, "Cidade " + i));
+            }
+
+            Console.WriteLine($"Test with {number} registers\n");
+
+            var swAny = new Stopwatch();
+            swAny.Start();
+            var any = funcionarios.Any(f => f.Nome == "Funcionario 99999");
+            swAny.Stop();
+            Console.WriteLine($"Any: {swAny.ElapsedTicks} ticks\n");
+
+            var swExists = new Stopwatch();
+            swExists.Start();
+            var exists = funcionarios.Exists(f => f.Nome == "Funcionario 99999");
+            swExists.Stop();
+            Console.WriteLine($"Exists: {swExists.ElapsedTicks} ticks\n");
+
+            var swCount = new Stopwatch();
+            swCount.Start();
+            var encontrou = funcionarios.Count(f => f.Nome == "Funcionario 99999") == 1;
+            swCount.Stop();
+            Console.WriteLine($"Count: {swCount.ElapsedTicks} ticks\n");
+
+            Console.WriteLine("--------------------------------------------------\n");
+        }
+    }
+}
---
+++ BenchmarkTests/Funcionario.cs
@@ -0,0 +1,15 @@
+﻿namespace BenchmarkTests
+{
+    public class Funcionario(string nome, int idade,
+                       double salario, DateTime dataNascimento,
+                       string endereco, string bairro, string cidade)
+    {
+        public string Nome { get; set; } = nome;
+        public int Idade { get; set; } = idade;
+        public double Salario { get; set; } = salario;
+        public DateTime DataNascimento { get; set; } = dataNascimento;
+        public string Endereco { get; set; } = endereco;
+        public string Bairro { get; set; } = bairro;
+        public string Cidade { get; set; } = cidade;
+    }
+}
